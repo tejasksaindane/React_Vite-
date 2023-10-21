fea28ac282e963c7155938396ce579f8fa8f083e
tejasksaindane <tejassaindane99@gmail.com>
2023-10-21 23:39:00 +0530

    minicontext

---
+++ minicontext/src/context/UserContext.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+
+//TODO: This is what we created a context and stored it in variable called UserContext.
+const UserContext = React.createContext()
+
+export default UserContext;
