2cc33325d2e8df5ffc67cbfefd16b8d09abd4cce
tejasksaindane <tejassaindane99@gmail.com>
2023-10-26 02:38:17 +0530

    Add Project 4 code

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// import { useState } from 'react'
+import "./App.css";
+
+const App = () => {
+  console.log(import.meta.env.VITE_APPWRITE_URL);
+  return (
+    <>
+      <h1>A Blog App in Appwrite.</h1>
+    </>
+  );
+};
+
+export default App;
+// 202832104
---
+++ src/appwrite/auth.js
@@ -0,0 +1,50 @@
+import config from "../config";
+import { Client, Account, ID } from "appwrite";
+
+export class AuthService {
+  client = new Client();
+  account;
+
+  constructor() {
+    this.client
+      .setEndpoint(config.appwriteUrl)
+      .setProject(config.appwriteProjectId);
+    this.account = new Account(this.client);
+  }
+
+  async createAccount({ email, password, name }) {
+    try {
+      const userAccount = await this.account.create(
+        ID.unique(),
+        email,
+        password,
+        name
+      );
+      if (userAccount) {
+        //call Another method
+      } else {
+        return userAccount;
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async login({ email, password }) {
+    try {
+      return await this.account.createEmailSession(email, password);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async getCurrentUser() {
+    try {
+    } catch (error) {
+      console.log("appwrite service");
+    }
+  }
+}
+const AuthService = new AuthService();
+
+export default AuthService;
