ebf5eeeb87dbcf596e6043baeefc0b3def5532f8
tejasksaindane <tejassaindane99@gmail.com>
2023-10-28 17:29:25 +0530

    Second Commit

---
+++ src/appwrite/conf.js
@@ -0,0 +1,35 @@
+import config from "../config";
+import { Client, ID, Databases, Storage, Query } from "appwrite";
+
+export class Service {
+    client = new Client();
+    databases;
+    bucket;
+    constructor() {
+        this.client
+            .setEndpoint(config.appwriteUrl)
+            .setProject(config.appwriteProjectId);
+        this.databases = new Databases(this.client);
+        this.bucket = new Storage(this.client);
+    }
+
+    async createPost({ title, slug, content, featuredImage, status, userId }) {
+        try {
+            return await this.databases.createDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug,
+                {
+                    title,content,featureImage,status,userId,
+                }
+            )
+        } catch (error) {
+            console.log("Appwrite Service :: createPost ::error", error);
+        }
+    }
+
+    
+}
+
+const service = new Service();
+export default service 
